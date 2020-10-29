36b5a090146f60a1e35dd9b3f0c48be7776beb55
miraclefish <yuyifei@sjtu.edu.cn>
2020-10-29 09:28:25 +0800

    Test V1

--- EEG_BECT/NewTest.py
+++ EEG_BECT/NewTest.py
@@ -1,9 +1,15 @@
 from BECTdetection import BECTdetect
 import os
+import pandas as pd
 
 path = './NewcsvData'
 filelist = os.listdir(path)
+indicators = []
 for i, filename in enumerate(filelist):
     filepath = os.path.join(path, filename)
     bect = BECTdetect(filepath = filepath, S_size=61, threshold=2, print_log=False)
+    indicators.append(bect.indicator)
     pass
+
+result = pd.DataFrame({'Name':filelist, 'indicator':indicators})
+result.to_csv('测试结果.csv')
