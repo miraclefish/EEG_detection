97a1a0cc1d98c941c2b0d56069e462146af95bd2
miraclefish <yuyifei@sjtu.edu.cn>
2021-02-25 16:44:49 +0800

    Segtest_demo0 update

--- EEG_BECT/AutoTH/Segtest_demo0.py
+++ EEG_BECT/AutoTH/Segtest_demo0.py
@@ -6,6 +6,22 @@ from SignalSegNet import SignalSegNet, Basicblock, Bottleneck
 from torch.utils.data import DataLoader
 from Dataset import BECTDataset
 
+def find_index_pair(label):
+    d_label = label[1:] - label[:-1]
+    index_S = np.where(d_label==1)[0]
+    index_E = np.where(d_label==-1)[0]
+
+    if index_E[0] < index_S[0]:
+        index_S.insert(0, -1)
+    elif index_S[-1] > index_E[-1]:
+        index_E.append(len(d_label))
+    
+    assert len(index_S)==len(index_E)
+    index_pair = [np.array([[start, end]]) for start, end in zip(index_S, index_E)]
+    index_pair = np.concatenate(index_pair, axis=0)
+    index_pair = index_pair + 1
+    return index_pair
+
 def inital_net(model_root, epoch=0):
 
     net = SignalSegNet(Basicblock, [2,2,2,2,2])
@@ -38,23 +54,38 @@ def test_demo(net, data_type, num):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     x, label = getdata(type=data_type, num=num)
+    data = x.cpu().data.numpy()
+    label = label.cpu().data.numpy()
+
     net = net.to(device)
     x = x.to(device)
-    label = label.to(device)
 
     output = net(x=x)
-    pred = torch.argmax(output, dim=1)
-    pred = pred.squeeze(dim=0)
+    output = torch.argmax(output, dim=1)
+    output = output.squeeze(dim=0)
+
+    pred = output.cpu().data.numpy()
+
+    # 寻找输出为1的起始index
+    label_index_pair = find_index_pair(label)
+    pred_index_pair = find_index_pair(pred)
 
     fig, ax = plt.subplots(figsize=(15,5))
+    ax.subplot(3,1,1)
+    ax.plot(data)
+    ax.title("Original data")
 
-    ax.plot(label.cpu().data.numpy(), label='Label')
-    ax.plot(pred.cpu().data.numpy(), label='Pred')
-    # ax.set_title('Test_demo with MSE = {:.2f}'.format(mse), fontsize=18)
-    ax.set_title('{}_demo'.format(data_type))
-    ax.set_xlabel('Samples', fontsize=18)
-    ax.set_ylabel('spike', fontsize=18)
-    ax.legend()
+    ax.subplot(3,1,2)
+    ax.plot(data)
+    for ind in label_index_pair:
+        plt.plot(np.arange(ind[0], ind[1]), data[ind[0]:ind[1]],c="r")
+    ax.title("BECT label segment")
+
+    ax.subplot(3,1,3)
+    ax.plot(data)
+    for ind in pred_index_pair:
+        plt.plot(np.arange(ind[0], ind[1]), data[ind[0]:ind[1]],c="g")
+    ax.title("BECT pred segment")
 
     # print('epoch: %d, loss of the %s dataset: %f' % (epoch, dataset_name, mse))
 
